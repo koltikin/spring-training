a3ad1641d02a4fcf7907dec524a42936bdb5ee05
ziya <koltikin210@gmail.com>
2023-08-18 11:31:10 +0300

    registration form is done for the mentor register

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/NewMentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/NewMentorController.java
@@ -1,10 +1,12 @@
 package com.cydeo.controller;
 
 import com.cydeo.model.Gender;
+import com.cydeo.model.Mentor;
 import com.cydeo.model.NewMentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -28,20 +30,21 @@ public class NewMentorController {
         model.addAttribute("gender",genderList);
         model.addAttribute("batchList",BATCHES);
         model.addAttribute("mentor",new NewMentor());
-
-        return "mentor/newRegister";
+        return "mentor/newRegister"; // path name of the Html file
     }
 
     @PostMapping("/confirm")
-    public String submitForm(){
+    public String submitForm(@ModelAttribute("mentor")NewMentor mentor){
+        System.out.println(mentor);
 
-        return "mentor/confirmation";
+        return "redirect:/mentor/newregister"; // mapping name from another Mapping;
     }
 
     @GetMapping("/confirm")
-    public String submitForm2(){
+    public String submitForm2(@ModelAttribute("mentor")NewMentor mentor){
+        System.out.println(mentor);
 
-        return "mentor/confirmation";
+        return "redirect:/mentor/newregister";
     }
 
 
