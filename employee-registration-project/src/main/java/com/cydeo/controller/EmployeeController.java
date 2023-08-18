f027f9889106f27df97b2eab9ebfb73aaa5ee1df
ziya <koltikin210@gmail.com>
2023-08-18 19:15:18 +0300

    connection between UI and controller is done

--- employee-registration-project/src/main/java/com/cydeo/controller/EmployeeController.java
+++ employee-registration-project/src/main/java/com/cydeo/controller/EmployeeController.java
@@ -1,17 +1,21 @@
 package com.cydeo.controller;
 
-import lombok.Getter;
+import com.cydeo.model.Employee;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import static com.cydeo.bootstrap.DataGenerator.STATES;
 
 @Controller
 @RequestMapping("/employee")
 public class EmployeeController {
 
     @GetMapping("/register")
-    public String createEmployee(){
+    public String createEmployee(Model model){
 
+        model.addAttribute("stateList",STATES);
+        model.addAttribute("employee",new Employee());
 
         return "/employee/employee-create";
     }
