c2588abf47a07030ae9f99da08efd4ad06285d11
ziya <koltikin210@gmail.com>
2023-08-20 12:38:56 +0300

    validations for birthDate, email, password fields are added

--- employee-registration-project/src/main/java/com/cydeo/model/Employee.java
+++ employee-registration-project/src/main/java/com/cydeo/model/Employee.java
@@ -2,8 +2,7 @@ package com.cydeo.model;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Data
@@ -14,10 +13,18 @@ public class Employee {
     @NotBlank
     @Size(max = 20,min = 2)
     private String firstName ,lastName;
-//    private String lastName;
+
+    @NotNull(message = "Birth Date is Required")
+    @Past(message = "Birth date must be in the past")
     @DateTimeFormat(pattern = "yyyy-MM-dd")  // Thymeleaf accepts yyyy-MM-dd, but the localDate accepts mm-MM-yyyy
     private LocalDate birthDate;
+
+    @Email
     private String email;
+    @NotBlank
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            message = "Password must be at least 8 characters long and contain at least one uppercase letter," +
+                    " one lowercase letter, one digit, and one special character.")
     private String password;
     private String address;
     private String address2;
