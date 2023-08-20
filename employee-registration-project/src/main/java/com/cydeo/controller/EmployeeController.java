8a203ab02be1cd76bc997bf8172524a5ddcac34c
ziya <koltikin210@gmail.com>
2023-08-20 03:25:41 +0300

    backed validation added

--- employee-registration-project/src/main/java/com/cydeo/controller/EmployeeController.java
+++ employee-registration-project/src/main/java/com/cydeo/controller/EmployeeController.java
@@ -6,11 +6,13 @@ import com.cydeo.service.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.text.DateFormat;
 
 import static com.cydeo.bootstrap.DataGenerator.STATES;
@@ -31,7 +33,12 @@ public class EmployeeController {
         return "/employee/employee-create";
     }
     @PostMapping("/insert")
-    public String registerConfirm(@ModelAttribute("employee") Employee employee){
+    public String registerConfirm(@ModelAttribute("employee") @Valid Employee employee, BindingResult bindingResult, Model model){
+
+        if(bindingResult.hasErrors()){
+            model.addAttribute("stateList", STATES);
+            return "/employee/employee-create";
+        }
 
         employeeService.saveEmployee(employee);
 
--- employee-registration-project/src/main/java/com/cydeo/model/Employee.java
+++ employee-registration-project/src/main/java/com/cydeo/model/Employee.java
@@ -2,15 +2,20 @@ package com.cydeo.model;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Employee {
+
+    @NotBlank
+    @Size(max = 20,min = 2)
     private String firsName;
     private String lastName;
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")  // Thymeleaf accepts yyyy-MM-dd, but the localDate accepts mm-MM-yyyy
     private LocalDate birthDate;
     private String email;
     private String password;
@@ -19,5 +24,4 @@ public class Employee {
     private String city;
     private String state;
     private String zipCode;
-
 }
