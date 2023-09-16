07a38c7fcdadbf53a6d87e3ad6c577637f2fd4fc
ziya <koltikin210@gmail.com>
2023-09-16 21:39:15 +0300

    spring.jpa.defer-datasource-initialization=true this property do first create table than insert the data

--- spring-11-Data-TableCreation/src/main/resources/data.sql
+++ spring-11-Data-TableCreation/src/main/resources/data.sql
@@ -1,2 +1,8 @@
 insert into EMPLOYEES(id,name)
 values (1,'Ziya');
+
+insert into student(id,email,first_name,last_name)
+values (1,'koltikin210@gmail.com','ziya','kasgari');
+
+insert into student(id,email,first_name,last_name)
+values (2,'yusuf@gmail.com','yusuf','abdullah');
