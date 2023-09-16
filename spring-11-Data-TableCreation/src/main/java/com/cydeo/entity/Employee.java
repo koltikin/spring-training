db16b484418adcfc2bea0f86e62dec105220c54b
ziya <koltikin210@gmail.com>
2023-09-16 21:46:33 +0300

    change the table name with @Table annotation

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Employee.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Employee.java
@@ -2,8 +2,10 @@ package com.cydeo.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "employees")
 public class Employee {
     @Id
     private int id;
