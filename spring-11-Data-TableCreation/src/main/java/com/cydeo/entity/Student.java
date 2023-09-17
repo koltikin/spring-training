1e742836df5169eb0bfafccd0c5522f1c18f434a
ziya <koltikin210@gmail.com>
2023-09-17 07:23:07 +0300

    change the column name with @Column(name = "studentFirstName") annotation

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Student.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Student.java
@@ -3,13 +3,17 @@ package com.cydeo.entity;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "students")
+@Table(name = "Students")
 public class Student {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+    @Column(name = "studentFirstName")
     private String firstName;
+    @Column(name = "studentLastName")
     private String lastName;
+    @Column(name = "studentEmail")
     private String email;
+
 }
--- spring-11-Data-TableCreation/src/main/resources/data.sql
+++ spring-11-Data-TableCreation/src/main/resources/data.sql
@@ -1,8 +1,8 @@
 insert into EMPLOYEES(id,name)
 values (1,'Ziya');
 
-insert into students(id,email,first_name,last_name)
+insert into students(id,student_email,student_first_name,student_last_name)
 values (1,'koltikin210@gmail.com','ziya','kasgari');
 
-insert into students(id,email,first_name,last_name)
+insert into students(id,student_email,student_first_name,student_last_name)
 values (2,'yusuf@gmail.com','yusuf','abdullah');
