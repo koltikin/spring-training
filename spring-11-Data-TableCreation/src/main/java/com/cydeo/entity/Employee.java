a509d6a5761566da2d7dfed269a1eb1624db94be
ziya <koltikin210@gmail.com>
2023-09-17 06:45:57 +0300

    give the unique id with @GeneratedValue(strategy = GenerationType.AUTO) annotation

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Employee.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Employee.java
@@ -1,13 +1,12 @@
 package com.cydeo.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "employees")
 public class Employee {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String name;
 }
--- spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Student.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Student.java
@@ -1,14 +1,13 @@
 package com.cydeo.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "students")
 public class Student {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String firstName;
     private String lastName;
