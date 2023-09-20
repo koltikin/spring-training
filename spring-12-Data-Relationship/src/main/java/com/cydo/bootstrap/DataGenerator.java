ad431cdd684eb9729f837b34f5f599db74812c3f
ziya <koltikin210@gmail.com>
2023-09-20 18:25:08 +0300

    data added to merchant table and merchant sat to the payments

--- spring-12-Data-Relationship/src/main/java/com/cydo/bootstrap/DataGenerator.java
+++ spring-12-Data-Relationship/src/main/java/com/cydo/bootstrap/DataGenerator.java
@@ -1,8 +1,10 @@
 package com.cydo.bootstrap;
 
+import com.cydo.entity.Merchant;
 import com.cydo.entity.Payment;
 import com.cydo.entity.PaymentDetail;
 import com.cydo.enums.PaymentStatus;
+import com.cydo.repository.MerchantRepository;
 import com.cydo.repository.PaymentRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
@@ -17,6 +19,7 @@ import java.util.Arrays;
 public class DataGenerator implements CommandLineRunner {
 
     private final PaymentRepository paymentRepository;
+    private final MerchantRepository merchantRepository;
     @Override
     public void run(String... args) throws Exception {
 
@@ -28,11 +31,20 @@ public class DataGenerator implements CommandLineRunner {
         Payment payment2 = new Payment(LocalDate.of(2022,4,25),new BigDecimal("100000"), PaymentStatus.FAILURE);
         PaymentDetail paymentDetail2 = new PaymentDetail(new BigDecimal("90000"),new BigDecimal("5000"),LocalDate.of(2022,4,29));
 
+        Merchant merchant1 = new Merchant("AmazonSubMerchant","M123",new BigDecimal("0.25"),new BigDecimal("3.25"),5);
+
+        merchantRepository.save(merchant1);
+
+        payment1.setMerchant(merchant1);
+        payment2.setMerchant(merchant1);
+
         payment2.setPaymentDetail(paymentDetail2);
 
         paymentRepository.saveAll(Arrays.asList(payment1,payment2));
 //        System.out.println("payment 2**************>: "+paymentRepository.findById(2L));
         paymentRepository.delete(payment1);
+
+
 
     }
 }
