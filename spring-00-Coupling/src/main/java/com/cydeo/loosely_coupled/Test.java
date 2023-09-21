d3c75654fa6918d64e5da344e7249d0053ae0c50
ziya <koltikin210@gmail.com>
2023-09-21 17:54:49 +0300

    loosely coupling structure is completed

--- spring-00-Coupling/src/main/java/com/cydeo/loosely_coupled/Test.java
+++ spring-00-Coupling/src/main/java/com/cydeo/loosely_coupled/Test.java
@@ -3,7 +3,6 @@ package com.cydeo.loosely_coupled;
 import com.cydeo.loosely_coupled.caseFactory.Case;
 import com.cydeo.loosely_coupled.caseFactory.DellCase;
 import com.cydeo.loosely_coupled.monitorFactory.AcerMonitor;
-import com.cydeo.loosely_coupled.monitorFactory.LgMonitor;
 import com.cydeo.loosely_coupled.monitorFactory.Monitor;
 import com.cydeo.loosely_coupled.motherBoardFactory.AsusMotherBoard;
 import com.cydeo.loosely_coupled.motherBoardFactory.MotherBoard;
