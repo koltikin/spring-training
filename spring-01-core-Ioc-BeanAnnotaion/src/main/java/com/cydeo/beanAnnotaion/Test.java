9adc4b88a420b736dc913b2794e449f14385f2b4
ziya <koltikin210@gmail.com>
2023-09-23 19:20:42 +0300

    create container use BeanFactory interface

--- spring-01-core-Ioc-BeanAnnotaion/src/main/java/com/cydeo/beanAnnotaion/Test.java
+++ spring-01-core-Ioc-BeanAnnotaion/src/main/java/com/cydeo/beanAnnotaion/Test.java
@@ -4,11 +4,13 @@ import com.cydeo.beanAnnotaion.casefactory.Case;
 import com.cydeo.beanAnnotaion.config.CaseConfig;
 import com.cydeo.beanAnnotaion.config.ComputerConfig;
 import com.cydeo.beanAnnotaion.monitorfactory.Monitor;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Test {
     public static void main(String[] args) {
+        /** create container use ApplicationContext interface */
         ApplicationContext context = new AnnotationConfigApplicationContext(ComputerConfig.class);
 
 //        if there is more than one config class we can use like this
@@ -20,6 +22,15 @@ public class Test {
 
         Case appleCase = context1.getBean("appleCase", Case.class);
         appleCase.pressPowerButton();
+
+
+        /** create container use BeanFactory interface */
+
+        BeanFactory container = new AnnotationConfigApplicationContext(ComputerConfig.class);
+
+        Case appleCaseWithBeanFactory = container.getBean("DellCase",Case.class);
+
+        appleCaseWithBeanFactory.pressPowerButton();
 
     }
 
