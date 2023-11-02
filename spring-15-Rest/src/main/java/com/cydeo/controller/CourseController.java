ef37117a918e40e809e59b97af6fb1a73f6193c2
ziya <koltikin210@gmail.com>
2023-11-02 23:03:46 +0300

    controller method created and some GetMapping created

--- spring-15-Rest/src/main/java/com/cydeo/controller/CourseController.java
+++ spring-15-Rest/src/main/java/com/cydeo/controller/CourseController.java
@@ -4,6 +4,7 @@ import com.cydeo.dto.CourseDTO;
 import com.cydeo.service.CourseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +18,16 @@ public class CourseController {
 
     @GetMapping
     public List<CourseDTO> getAllCourses(){
-
         return courseService.getCourses();
+    }
+
+    @GetMapping("/{id}")
+    public CourseDTO getCourse(@PathVariable("id") Long courserId){
+        return courseService.getCourseById(courserId);
+    }
+    @GetMapping("category/{name}")
+    public List<CourseDTO> getCoursesByCategory(@PathVariable("name") String category){
+        return courseService.getCoursesByCategory(category);
 
     }
 }
