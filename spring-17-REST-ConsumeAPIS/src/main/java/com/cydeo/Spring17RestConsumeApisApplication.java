00ad883671b443a6cd8fbd1ab11f6a156d2aa552
ziya <koltikin210@gmail.com>
2023-11-03 20:58:55 +0300

    consuming APIs initial commit

---
+++ spring-17-REST-ConsumeAPIS/src/main/java/com/cydeo/Spring17RestConsumeApisApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring17RestConsumeApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring17RestConsumeApisApplication.class, args);
+	}
+
+}
