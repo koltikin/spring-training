5af1c8165c2b2f157fe1a72a2df36e8336e19ad9
ziya <koltikin210@gmail.com>
2023-11-05 11:56:11 +0300

    students endpoint is created

--- spring-18-REST-SchoolLab/src/main/java/com/cydeo/controller/SchoolController.java
+++ spring-18-REST-SchoolLab/src/main/java/com/cydeo/controller/SchoolController.java
@@ -1,8 +1,14 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.ResponseWrapper;
+import com.cydeo.dto.StudentDTO;
 import com.cydeo.dto.TeacherDTO;
+import com.cydeo.service.StudentService;
 import com.cydeo.service.TeacherService;
 import lombok.RequiredArgsConstructor;
+import org.apache.catalina.filters.AddDefaultCharsetFilter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +19,21 @@ import java.util.List;
 @RequestMapping("api/v1")
 public class SchoolController {
     private final TeacherService teacherService;
+    private final StudentService studentService;
 
     @GetMapping("/teachers")
     public List<TeacherDTO> getAllTeachers(){
         List<TeacherDTO> teachers = teacherService.findAll();
         return teachers;
+    }
+    @GetMapping("/students")
+    public ResponseEntity<ResponseWrapper> getAllStudents(){
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .header("version","api.v1")
+                .header("operation","retrieve all students")
+                .body(new ResponseWrapper("students are successfully retrieved",
+                        studentService.findAll()));
+
     }
 }
