658d427fa74ba2735f320269ad06f8a927a36e04
ziya <koltikin210@gmail.com>
2023-11-06 11:41:19 +0300

    adding opnApi-ui dependency

--- spring-19-REST-OpenAPI3/src/main/java/com/cydeo/controller/AccountController.java
+++ spring-19-REST-OpenAPI3/src/main/java/com/cydeo/controller/AccountController.java
@@ -2,9 +2,6 @@ package com.cydeo.controller;
 
 import com.cydeo.dto.AccountDTO;
 import com.cydeo.service.AccountService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.tags.Tags;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +12,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/accounts")
-@Tag(name = "Account", description = "Account CRUD Operations")
-@Tags(value = {@Tag(name = "Account", description = "Account CRUD Operations"),
-        @Tag(name = "Account2", description = "Account2 CRUD Operations")})
 public class AccountController {
 
     private final AccountService accountService;
@@ -27,13 +21,11 @@ public class AccountController {
     }
 
     @GetMapping("/list")
-    @Operation(summary = "Read all accounts")
     public ResponseEntity<List<AccountDTO>> getAllAccounts() {
         return ResponseEntity.ok(accountService.getAccounts());
     }
 
     @GetMapping
-    @Operation(summary = "Read an account by id")
     public ResponseEntity<AccountDTO> getById(@RequestParam("id") Long id) throws Exception {
         return ResponseEntity.ok(accountService.getById(id));
     }
