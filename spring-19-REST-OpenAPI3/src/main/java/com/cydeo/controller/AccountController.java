bcb1f1d5d19613f91c0aea2d0f17ee1f2b3449ad
ziya <koltikin210@gmail.com>
2023-11-07 23:06:02 +0300

    AccountController config done

--- spring-19-REST-OpenAPI3/src/main/java/com/cydeo/controller/AccountController.java
+++ spring-19-REST-OpenAPI3/src/main/java/com/cydeo/controller/AccountController.java
@@ -3,31 +3,35 @@ package com.cydeo.controller;
 import com.cydeo.dto.AccountDTO;
 import com.cydeo.dto.ResponseWrapper;
 import com.cydeo.service.AccountService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/accounts")
+@Tag(name = "Account",description = "Account CRUD operations")
 public class AccountController {
     private final AccountService accountService;
 
     @GetMapping("/list")
+    @Operation(summary = "Read All Accounts")
     public ResponseEntity<List<AccountDTO>> getAllAccounts() {
         return ResponseEntity.ok(accountService.getAccounts());
     }
     @GetMapping
+    @Operation(summary = "Read An Account By Id")
     public ResponseEntity<AccountDTO> getById(@RequestParam("id") Long id) throws Exception {
         return ResponseEntity.ok(accountService.getById(id));
     }
 
     @PostMapping
+    @Operation(summary = "Create an Account")
     public ResponseEntity<ResponseWrapper> createAccount(@RequestBody AccountDTO accountDTO) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(ResponseWrapper.builder()
@@ -38,6 +42,7 @@ public class AccountController {
     }
 
     @DeleteMapping
+    @Operation(summary = "Delete an Account")
     public ResponseEntity<Void> deleteById(@RequestParam("id") Long id){
         accountService.deleteById(id);
         return ResponseEntity.noContent().build();
