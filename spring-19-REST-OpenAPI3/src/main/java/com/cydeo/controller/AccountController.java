1d44a819bb6d89bdd6a6695662544411fc148972
ziya <koltikin210@gmail.com>
2023-11-07 23:51:44 +0300

    @ApiResponse config

--- spring-19-REST-OpenAPI3/src/main/java/com/cydeo/controller/AccountController.java
+++ spring-19-REST-OpenAPI3/src/main/java/com/cydeo/controller/AccountController.java
@@ -4,8 +4,12 @@ import com.cydeo.dto.AccountDTO;
 import com.cydeo.dto.ResponseWrapper;
 import com.cydeo.service.AccountService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +25,12 @@ public class AccountController {
 
     @GetMapping("/list")
     @Operation(summary = "Read All Accounts")
+    @ApiResponse(responseCode = "200",
+                 description = "successful response",
+                 content = @Content(mediaType = "application/json"))
+    @ApiResponse(responseCode = "404",
+            description = "Accounts not found",
+            content = @Content(mediaType = "application/json"))
     public ResponseEntity<List<AccountDTO>> getAllAccounts() {
         return ResponseEntity.ok(accountService.getAccounts());
     }
@@ -31,11 +41,14 @@ public class AccountController {
     }
 
     @PostMapping
+    @ApiResponse(responseCode = "201",
+            description = "successful response",
+            content = @Content(mediaType = "application/json"))
     @Operation(summary = "Create an Account")
     public ResponseEntity<ResponseWrapper> createAccount(@RequestBody AccountDTO accountDTO) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(ResponseWrapper.builder()
-                        .message("the account with successfully created")
+                        .message("new account successfully created")
                         .status(HttpStatus.ACCEPTED)
                         .statusCode(HttpStatus.CREATED.value())
                         .data(accountService.createAccount(accountDTO)).build());
