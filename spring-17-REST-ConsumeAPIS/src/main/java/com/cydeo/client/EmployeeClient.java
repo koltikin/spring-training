f8e73960c2677bf817dc2480bb63d445a207b13b
ziya <koltikin210@gmail.com>
2023-11-21 20:12:51 +0300

    spring-21 AOP initial commit

--- spring-17-REST-ConsumeAPIS/src/main/java/com/cydeo/client/EmployeeClient.java
+++ spring-17-REST-ConsumeAPIS/src/main/java/com/cydeo/client/EmployeeClient.java
@@ -5,7 +5,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-import java.util.List;
 
 @FeignClient(url = "https://dummyapi.io",name = "EMPLOYEE-CLIENT")
 public interface EmployeeClient {
