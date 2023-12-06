fb236543e4c35b02a96384efb7907de9802acb7c
ziya <koltikin210@gmail.com>
2023-12-06 17:14:17 +0300

    method Source

--- spring-22-UnitTesting-JUnit5/src/test/java/CalculatorParameterizedTest.java
+++ spring-22-UnitTesting-JUnit5/src/test/java/CalculatorParameterizedTest.java
@@ -1,6 +1,6 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
 
 public class CalculatorParameterizedTest {
     @ParameterizedTest
@@ -13,5 +13,32 @@ public class CalculatorParameterizedTest {
     @ValueSource(ints={2,6,9})
     void testCase2(int num){
         Assertions.assertEquals(0, num % 3);
+    }
+    @ParameterizedTest
+    @ValueSource(strings={"Java", "JS","TS"})
+    @EmptySource
+    void testCase3(String args){
+        Assertions.assertFalse(args.isEmpty());
+    }
+    @ParameterizedTest
+    @ValueSource(strings={"Java", "JS","TS"})
+    @NullSource
+    void testCase4(String args){
+        Assertions.assertFalse(args.isEmpty());
+    }
+    @ParameterizedTest
+    @ValueSource(strings={"Java", "JS","TS"})
+    @NullAndEmptySource
+    void testCase5(String args){
+        Assertions.assertFalse(args.isEmpty());
+    }
+
+    @ParameterizedTest
+    @MethodSource("StringProvider")
+    void testCase6(String arg){
+        Assertions.assertFalse(arg.isEmpty());
+    }
+    static String[] StringProvider(){
+        return new String[]{"Java","JS","TS"};
     }
 }
