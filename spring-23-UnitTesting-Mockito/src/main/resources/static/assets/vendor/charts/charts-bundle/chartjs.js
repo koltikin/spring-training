d4ec6ecbc4ef19f5d43fb1ad117b8f8c4cd53caf
ziya <koltikin210@gmail.com>
2023-12-06 21:05:03 +0300

    initial commit

--- spring-23-UnitTesting-Mockito/src/main/resources/static/assets/vendor/charts/charts-bundle/chartjs.js
+++ spring-23-UnitTesting-Mockito/src/main/resources/static/assets/vendor/charts/charts-bundle/chartjs.js
@@ -2,10 +2,11 @@
         "use strict";
         $(function() {
 
-                if ($('#chartjs_line').length) {
+                let myChart;
+            if ($('#chartjs_line').length) {
                     var ctx = document.getElementById('chartjs_line').getContext('2d');
 
-                    var myChart = new Chart(ctx, {
+                    myChart = new Chart(ctx, {
                             type: 'line',
 
                             data: {
@@ -55,7 +56,7 @@
                                     }]
                                 }
                             }
-                        
+
 
 
                     });
@@ -64,66 +65,47 @@
 
             if ($('#chartjs_bar').length) {
                 var ctx = document.getElementById("chartjs_bar").getContext('2d');
-                var myChart = new Chart(ctx, {
+                myChart = new Chart(ctx, {
                     type: 'bar',
                     data: {
                         labels: ["M", "T", "W", "R", "F", "S", "S"],
                         datasets: [{
                             label: 'Almonds',
                             data: [12, 19, 3, 17, 28, 24, 7],
-                           backgroundColor: "rgba(89, 105, 255,0.5)",
-                                    borderColor: "rgba(89, 105, 255,0.7)",
+                            backgroundColor: "rgba(89, 105, 255,0.5)",
+                            borderColor: "rgba(89, 105, 255,0.7)",
                             borderWidth: 2
                         }, {
                             label: 'Cashew',
                             data: [30, 29, 5, 5, 20, 3, 10],
-                           backgroundColor: "rgba(255, 64, 123,0.5)",
-                                    borderColor: "rgba(255, 64, 123,0.7)",
+                            backgroundColor: "rgba(255, 64, 123,0.5)",
+                            borderColor: "rgba(255, 64, 123,0.7)",
                             borderWidth: 2
                         }]
                     },
                     options: {
                         scales: {
-                            yAxes: [{
-
-                            }]
+                            yAxes: [{}]
                         },
-                             legend: {
-                        display: true,
-                        position: 'bottom',
+                        legend: {
+                            display: true,
+                            position: 'bottom',
 
-                        labels: {
-                            fontColor: '#71748d',
-                            fontFamily: 'Circular Std Book',
-                            fontSize: 14,
-                        }
-                    },
-
-                    scales: {
-                        xAxes: [{
-                            ticks: {
-                                fontSize: 14,
-                                fontFamily: 'Circular Std Book',
+                            labels: {
                                 fontColor: '#71748d',
-                            }
-                        }],
-                        yAxes: [{
-                            ticks: {
-                                fontSize: 14,
                                 fontFamily: 'Circular Std Book',
-                                fontColor: '#71748d',
+                                fontSize: 14,
                             }
-                        }]
+                        },
                     }
-                }
 
-                    
+
                 });
             }
 
             if ($('#chartjs_radar').length) {
                 var ctx = document.getElementById("chartjs_radar");
-                var myChart = new Chart(ctx, {
+                myChart = new Chart(ctx, {
                     type: 'radar',
                     data: {
                         labels: ["M", "T", "W", "T", "F", "S", "S"],
@@ -142,7 +124,7 @@
                         }]
                     },
                     options: {
-                       
+
                              legend: {
                         display: true,
                         position: 'bottom',
@@ -154,7 +136,7 @@
                         }
                     },
 
-                    
+
                 }
 
                 });
@@ -163,7 +145,7 @@
 
             if ($('#chartjs_polar').length) {
                 var ctx = document.getElementById("chartjs_polar").getContext('2d');
-                var myChart = new Chart(ctx, {
+                myChart = new Chart(ctx, {
                     type: 'polarArea',
                     data: {
                         labels: ["M", "T", "W", "T", "F", "S", "S"],
@@ -181,7 +163,7 @@
                         }]
                     },
                     options: {
-                        
+
                              legend: {
                         display: true,
                         position: 'bottom',
@@ -193,7 +175,7 @@
                         }
                     },
 
-                    
+
                 }
                 });
             }
@@ -201,7 +183,7 @@
 
             if ($('#chartjs_pie').length) {
                 var ctx = document.getElementById("chartjs_pie").getContext('2d');
-                var myChart = new Chart(ctx, {
+                myChart = new Chart(ctx, {
                     type: 'pie',
                     data: {
                         labels: ["M", "T", "W", "T", "F", "S", "S"],
@@ -230,7 +212,7 @@
                         }
                     },
 
-                    
+
                 }
                 });
             }
@@ -238,7 +220,7 @@
 
             if ($('#chartjs_doughnut').length) {
                 var ctx = document.getElementById("chartjs_doughnut").getContext('2d');
-                var myChart = new Chart(ctx, {
+                myChart = new Chart(ctx, {
                     type: 'doughnut',
                     data: {
                         labels: ["M", "T", "W", "T", "F", "S", "S"],
@@ -268,7 +250,7 @@
                         }
                     },
 
-                    
+
                 }
 
                 });
